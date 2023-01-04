09dd64c9f9bad66168cae46eeedde2a0a1b21a69
22MH1A04J3 <119720544+22MH1A04J3@users.noreply.github.com>
2023-01-04 19:59:04 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int c,flag=0;     scanf("%d",&c);     for(i=0;i<n;i++)     {         if(c==a[i])         {             flag=1;             break;         }     }     if(flag==1)     {         printf("True");     }     else     {         printf("False");     }     }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c,flag=0;
+    scanf("%d",&c);
+    for(i=0;i<n;i++)
+    {
+        if(c==a[i])
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    }
+        
